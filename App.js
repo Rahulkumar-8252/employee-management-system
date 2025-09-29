5c4d4bb577de2f1de147c228687f3717b384b4ed
RAHUL KUMAR <rahul2005kgr@gmail.com>
2025-09-29 19:29:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeList from "./components/EmployeeList";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center mt-4">Employee Management System</h1>
+      <EmployeeList />
+    </div>
+  );
+}
+
+export default App;
